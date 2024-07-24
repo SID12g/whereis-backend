1f88f7a10bd3b5f47ea971dbe5f3a4c7456c2d58
SID12g <sid12g@naver.com>
2024-07-24 11:52:05 +0900

    init

---
+++ src/test/java/com/sid12g/whereis/WhereisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid12g.whereis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
